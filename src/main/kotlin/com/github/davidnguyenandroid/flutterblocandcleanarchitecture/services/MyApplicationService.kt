b24f7f284efbc5040585afd26ed0e8cc5c1aeb52
GitHub Action <action@github.com>
2022-11-07 02:25:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/davidnguyenandroid/flutterblocandcleanarchitecture/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/davidnguyenandroid/flutterblocandcleanarchitecture/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.davidnguyenandroid.flutterblocandcleanarchitecture.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.davidnguyenandroid.flutterblocandcleanarchitecture.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/davidnguyenandroid/flutterblocandcleanarchitecture/services/MyProjectService.kt
+++ src/main/kotlin/com/github/davidnguyenandroid/flutterblocandcleanarchitecture/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.davidnguyenandroid.flutterblocandcleanarchitecture.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.davidnguyenandroid.flutterblocandcleanarchitecture.MyBundle
 
 class MyProjectService(project: Project) {
 
